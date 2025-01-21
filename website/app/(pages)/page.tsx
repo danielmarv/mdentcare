f53894d67e4f6392b5b9e0878e940e05b88284ca
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2025-01-21 08:27:13 -0800

    initial commite

---
+++ website/app/(pages)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='flex justify-center items-center h-screen '>
+      <h2 className='text-4xl'>Welcome to Mdent Care</h2>
+    </div>
+  )
+}
